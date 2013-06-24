b333d2147eca16fa3edb812e5039912f68bc60f5
tmushayahama <tmushayahama@gmail.com>
2013-06-24 19:33:28 -0400

    Prototyped the login form

--- protected/modules/user/data/schema.mysql.sql
+++ protected/modules/user/data/schema.mysql.sql
@@ -1,5 +1,6 @@
 -- DROP USER 'questionaire'@'localhost';
 -- CREATE USER 'questionaire'@'localhost' IDENTIFIED BY '';
+Drop database if exists questionaire;
 CREATE DATABASE questionaire DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 -- GRANT ALL PRIVILEGES ON questionaire.* to 'questionaire'@'localhost' WITH GRANT OPTION;
 USE questionaire;
@@ -9,9 +10,6 @@ CREATE TABLE `que_users` (
   `username` varchar(20) NOT NULL,
   `password` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
-	`lastname` varchar(50) NOT NULL DEFAULT '',
-  `firstname` varchar(50) NOT NULL DEFAULT '',
-	`organisation` varchar(100) NOT NULL DEFAULT '',
   `activkey` varchar(128) NOT NULL DEFAULT '',
   `create_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `lastvisit_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -24,6 +22,42 @@ CREATE TABLE `que_users` (
   KEY `superuser` (`superuser`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
+CREATE TABLE `que_profiles` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(50) NOT NULL DEFAULT '',
+  `firstname` varchar(50) NOT NULL DEFAULT '',
+	`organization` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
+
+ALTER TABLE `que_profiles`
+  ADD CONSTRAINT `user_profile_id` FOREIGN KEY (`user_id`) REFERENCES `que_users` (`id`) ON DELETE CASCADE;
+
+CREATE TABLE `que_profiles_fields` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `varname` varchar(50) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `field_type` varchar(50) NOT NULL,
+  `field_size` varchar(15) NOT NULL DEFAULT '0',
+  `field_size_min` varchar(15) NOT NULL DEFAULT '0',
+  `required` int(1) NOT NULL DEFAULT '0',
+  `match` varchar(255) NOT NULL DEFAULT '',
+  `range` varchar(255) NOT NULL DEFAULT '',
+  `error_message` varchar(255) NOT NULL DEFAULT '',
+  `other_validator` varchar(5000) NOT NULL DEFAULT '',
+  `default` varchar(255) NOT NULL DEFAULT '',
+  `widget` varchar(255) NOT NULL DEFAULT '',
+  `widgetparams` varchar(5000) NOT NULL DEFAULT '',
+  `position` int(3) NOT NULL DEFAULT '0',
+  `visible` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `varname` (`varname`,`widget`,`visible`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+
+
+
+
 CREATE TABLE `que_projects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
@@ -32,3 +66,9 @@ CREATE TABLE `que_projects` (
 	`create_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+
+INSERT INTO `que_profiles_fields` (`id`, `varname`, `title`, `field_type`, `field_size`, `field_size_min`, `required`, `match`, `range`, `error_message`, `other_validator`, `default`, `widget`, `widgetparams`, `position`, `visible`) VALUES
+(1, 'lastname', 'Last Name', 'VARCHAR', 50, 3, 1, '', '', 'Incorrect Last Name (length between 3 and 50 characters).', '', '', '', '', 1, 3),
+(2, 'firstname', 'First Name', 'VARCHAR', 50, 3, 1, '', '', 'Incorrect First Name (length between 3 and 50 characters).', '', '', '', '', 0, 3),
+(3, 'organization', 'Organization', 'VARCHAR', 50, 3, 1, '', '', 'Incorrect First Name (length between 3 and 50 characters).', '', '', '', '', 0, 3);
--- protected/modules/user/views/user/registration.php
+++ protected/modules/user/views/user/registration.php
@@ -25,7 +25,7 @@ $this->breadcrumbs=array(
 
 	<p class="note"><?php echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></p>
 	
-	<?php echo $form->errorSummary(array($model)); ?>
+	<?php echo $form->errorSummary(array($model,$profile)); ?>
 	
 	<div class="row">
 	<?php echo $form->labelEx($model,'username'); ?>
@@ -54,6 +54,30 @@ $this->breadcrumbs=array(
 	<?php echo $form->error($model,'email'); ?>
 	</div>
 	
+<?php 
+		$profileFields=$profile->getFields();
+		if ($profileFields) {
+			foreach($profileFields as $field) {
+			?>
+	<div class="row">
+		<?php echo $form->labelEx($profile,$field->varname); ?>
+		<?php 
+		if ($widgetEdit = $field->widgetEdit($profile)) {
+			echo $widgetEdit;
+		} elseif ($field->range) {
+			echo $form->dropDownList($profile,$field->varname,Profile::range($field->range));
+		} elseif ($field->field_type=="TEXT") {
+			echo$form->textArea($profile,$field->varname,array('rows'=>6, 'cols'=>50));
+		} else {
+			echo $form->textField($profile,$field->varname,array('size'=>60,'maxlength'=>(($field->field_size)?$field->field_size:255)));
+		}
+		 ?>
+		<?php echo $form->error($profile,$field->varname); ?>
+	</div>	
+			<?php
+			}
+		}
+?>
 	<?php if (UserModule::doCaptcha('registration')): ?>
 	<div class="row">
 		<?php echo $form->labelEx($model,'verifyCode'); ?>
